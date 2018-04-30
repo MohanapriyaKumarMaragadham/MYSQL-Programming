c0a78430f1a38977cb510af7f52bad9ace67afa2
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-30 23:29:54 +0530

    Create and or.sql

---
+++ and or.sql
@@ -0,0 +1,10 @@
+create database school;
+create table student(id int(10), name varchar(25), age int(20));
+insert into student values(1,	'Vinodini',	25);
+insert into student values(2,	'Banu',    	27);
+insert into student values(3,	'kaushik', 	23);
+insert into student values(4,	'Praveen', 	25);
+insert into student values(5,	'Kamal',   	22);
+insert into student values(6,	'Malini',  	24);
+insert into student values(7,	'Ramesh',  	32);
+select *from student where age=32 or age=22;
